ea12b9814483cb802638630e474fb6346be931c4
ashishInfyom <ashish.nakrani@infyom.com>
2020-06-06 13:45:13 +0530

    feat: implemented chat UI.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,6 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('conversations') }}">Covnersations</a>
                 </div>
             </div>
         </div>
