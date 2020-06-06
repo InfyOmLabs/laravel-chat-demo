068d55b4d3e09f852fd807a005d8721dfc09a8cf
ashishInfyom <ashish.nakrani@infyom.com>
2020-06-06 14:05:04 +0530

    fix: generate php doc.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
         return view('home.index');
